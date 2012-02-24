d2b4ead08d211ad65377833ece0617e77925dfc6
star_watcher13 <star_watcher13@yahoo.com>
2012-02-24 20:04:30 +0000

    2

---
+++ comments/emoticoane comment form.js
@@ -0,0 +1,51 @@
+<script type='text/javascript'> 
+//<![CDATA[
+function moreSmilies() {
+    document.getElementById('smiley-more').style.display = 'inline';
+    document.getElementById('smiley-toggle').innerHTML = '<a href="javascript:lessSmilies()">Restrange lista</a></span>';
+}
+function lessSmilies() {
+    document.getElementById('smiley-more').style.display = 'none';
+    document.getElementById('smiley-toggle').innerHTML = '<a href="javascript:moreSmilies()">Emoticoane</a>';
+}
+//]]>
+</script>
+<div class='emoticons'>
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-you-bad.gif'/>:()
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-sulky.gif'/>=d>
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-sneezing.gif'/>:r
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-shinning-new-glasses.gif'/>:B
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-sad-bye.gif'/>:h
+<span id='smiley-more' style='display: none;'>
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-reading.gif'/>:v 
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-phone-talk.gif'/>:c
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-perfectcouple.gif'/>:f
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-perfect.gif'/>:g
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-parfume.gif'/>:u
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-kissingcouple.gif'/>:k 
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-hula.gif'/>:i
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-hiding.gif'/>:a
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-happy-hi.gif'/>h-) 
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-happycouple.gif'/>l-)
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-group.gif'/>3-m
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-grandma.gif'/>8-)
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-funnycouple.gif'/>c-l
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-fuck-off.gif'/>m-)
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-formula-racing.gif'/>2-c
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-flower.gif'/>w-)
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-firstkiss.gif'/>2-h
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-fight.gif'/>2-l
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-dancingcouple.gif'/>2-)
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-cute.gif'/>;d
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-crying.gif'/>h-p
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-couple-argue.gif'/>2-(
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-clapping.gif'/>a-p
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-cheerleader.gif'/>j-)
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-beauty-care.gif'/>1-f
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-barmen-drink.gif'/>1-c
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-ashamed.gif'/>r-)
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-afraid-canlde.gif'/>s-p
+<img src='http://i1065.photobucket.com/albums/u389/treeofwisdom4/animated-smiley-404.gif'/>4-e
+</span>
+<span id='smiley-toggle'><a href='javascript:moreSmilies()'>Emoticoane</a></span>
+</div>
